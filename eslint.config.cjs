1a6167dff37610cdf5590395ff88e9615a3c207c
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-08 17:25:18 +0200

    chore: install Eslint with Airbnb rules (Thu, May 08, 2025 17:25:05 PM)

---
+++ eslint.config.cjs
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+      files: ["**/*.{ts,tsx}"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        parser: require("@typescript-eslint/parser"),
+      },
+      plugins: {
+        "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+        "react": require("eslint-plugin-react"),
+        "import": require("eslint-plugin-import"),
+        "jsx-a11y": require("eslint-plugin-jsx-a11y"),
+      },
+      rules: {
+        ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
+        ...require("eslint-plugin-react").configs.recommended.rules,
+        ...require("eslint-config-airbnb-base").rules,
+        ...require("eslint-config-airbnb/rules/react").rules,
+        ...require("eslint-config-airbnb/rules/react-a11y").rules,
+        "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+        "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      "react/react-in-jsx-scope": "off",
+      "import/prefer-default-export": "off",
+      },
+      settings: {
+        react: {
+          version: "detect", // ✅ Automatically detect React version
+        },
+      },
+    },
+  ];
+  
