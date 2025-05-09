ab1582ae3734b7a5aabbfb2253becee977bba81c
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-09 10:12:15 +0200

    chore: install prettier (Fri, May 09, 2025 10:12:21 AM)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,33 +1,36 @@
 module.exports = [
-    {
-      files: ["**/*.{ts,tsx}"],
-      languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        parser: require("@typescript-eslint/parser"),
-      },
-      plugins: {
-        "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-        "react": require("eslint-plugin-react"),
-        "import": require("eslint-plugin-import"),
-        "jsx-a11y": require("eslint-plugin-jsx-a11y"),
-      },
-      rules: {
-        ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
-        ...require("eslint-plugin-react").configs.recommended.rules,
-        ...require("eslint-config-airbnb-base").rules,
-        ...require("eslint-config-airbnb/rules/react").rules,
-        ...require("eslint-config-airbnb/rules/react-a11y").rules,
-        "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
-        "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-      "react/react-in-jsx-scope": "off",
-      "import/prefer-default-export": "off",
-      },
-      settings: {
-        react: {
-          version: "detect", // ✅ Automatically detect React version
-        },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: require('@typescript-eslint/parser'),
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      import: require('eslint-plugin-import'),
+      'jsx-a11y': require('eslint-plugin-jsx-a11y'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      ...require('@typescript-eslint/eslint-plugin').configs.recommended.rules,
+      ...require('eslint-plugin-react').configs.recommended.rules,
+      ...require('eslint-config-airbnb-base').rules,
+      ...require('eslint-config-airbnb/rules/react').rules,
+      ...require('eslint-config-airbnb/rules/react-a11y').rules,
+
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'react/react-in-jsx-scope': 'off',
+      'import/prefer-default-export': 'off',
+      'prettier/prettier': 'warn',
+      'no-irregular-whitespace': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect', // ✅ Automatically detect React version
       },
     },
-  ];
-  
+  },
+];
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,5 @@
 function App() {
-  return (
-    <h1>Hell   o    </h1>
-  );
+  return <h1>Hell o </h1>;
 }
 
-export 
-default App;
+export default App;
