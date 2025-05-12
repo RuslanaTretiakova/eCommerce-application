abe7a9c8b5d0b951f142e1fe96fa39f9e78d5ef0
Ruslana <tretiakovaruslana7@gmail.com>
2025-05-12 13:45:33 +0200

    feat: add RegistrationPage component with form(Mon, May 12, 2025, 01:45:00 PM)

---
+++ src/pages/registration-page/ui/RegistrationPage.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from 'react';
+import RegistrationForm from '../../../components/forms/registration/RegistrationForm.tsx';
+
+function RegistrationPage(): JSX.Element {
+  return <RegistrationForm />;
+}
+
+export default RegistrationPage;
