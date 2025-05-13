88147cbece4cc336902da6143dda93ff646dffde
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-13 17:16:47 +0200

    feat: add pages tabs to header (Tue, May 13, 2025 17:16:08 PM)

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../logo/logo";
 import './header.scss'
+import HeaderNavigation from "./header-navigation/header-navigation";
 
 const Header: React.FC<{}> = () => {
     return <header className="header">
     <Logo />
+    <HeaderNavigation />
     </header>
 }
 
