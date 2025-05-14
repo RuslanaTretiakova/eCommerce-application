529496ffe45f4785a88832242b3b55d293afcd1e
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-14 18:03:52 +0200

    feat: realization function to set navigation open for mobile devices (Tue, May 14, 2025 18:03:43 PM)

--- src/components/header/navigation-button/navigation-button.tsx
+++ src/components/header/navigation-button/navigation-button.tsx
@@ -2,7 +2,19 @@ import type React from "react";
 import './navigation-button.scss'
 
 const NavigationButton: React.FC<{}> = () => {
-    return <div className="navigation-button">
+    const setMenuOpen = () => {
+        let navigationOpen = document.querySelector('.navigation-open');
+        if(!navigationOpen){
+            const navigation = document.querySelector('.navigation');
+            navigation?.classList.add('navigation-open')
+        } else {
+            navigationOpen?.classList.remove('navigation-open')
+        }
+        console.log('Navigation button clicked');
+        // Add your toggle logic here (e.g. open menu)
+      };
+
+    return <div className="navigation-button" onClick={setMenuOpen}>
         <hr></hr>
         <hr></hr>
     </div>
