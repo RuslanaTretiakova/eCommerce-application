7acd29e9d5b87f40513df2a11f494bce9adb4ea6
Ruslana <tretiakovaruslana7@gmail.com>
2025-05-15 11:42:01 +0200

    feat: add layout and image for registration page(Thu, May 15, 2025, 11:41:00 AM)

--- src/pages/registration-page/ui/RegistrationPage.tsx
+++ src/pages/registration-page/ui/RegistrationPage.tsx
@@ -1,8 +1,19 @@
 import type { JSX } from 'react';
-import RegistrationForm from '../../../components/forms/registration/RegistrationForm.tsx';
+import RegistrationForm from '../../../components/forms/registration/RegistrationForm';
+
+import './_registration-page.scss';
 
 function RegistrationPage(): JSX.Element {
-  return <RegistrationForm />;
+  return (
+    <div className="registration-page">
+      <div className="registration-page__image">
+        <img src="./src/assets/images/register_page.jpeg.png" alt="Register illustration" />
+      </div>
+      <div className="registration-page__form">
+        <RegistrationForm />
+      </div>
+    </div>
+  );
 }
 
 export default RegistrationPage;
