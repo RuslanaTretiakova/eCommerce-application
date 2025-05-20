d922acf84eb0955a8d8f95760543530b30b049bc
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-20 20:04:27 +0200

    refactor: create router for main and App (Tue, May 20, 2025 20:03:11 PM)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,26 +3,19 @@ import AuthenticationPage from '../pages/authorisation-page/AuthenticationPage';
 import RegistratontionPageDraft from '../pages/authorisation-page/RegistrationPage-draft';
 import '../assets/styles/normalize.css';
 import '../assets/styles/global.scss';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to='/'></Link> 
-        <Link to='/registration'></Link>
+        <Link to="/" />
+        <Link to="/registration" />
       </nav>
-
       <Routes>
-        <Route path="/" element={<RegistratontionPageDraft />}/>
-        <Route path="/registration" element={<AuthenticationPage />}/>
+        <Route path="/" element={<RegistratontionPageDraft />} />
+        <Route path="/registration" element={<AuthenticationPage />} />
       </Routes>
-      {/* // <h1>Hell o </h1> */}
-      // {/* <RegistrationPage /> */}
-      {/* // <h2>auth</h2> */}
-      {/* // <AuthenticationPage /> */}
-      {/* // <h2>Reg Draft</h2> */}
-      {/* // <RegistratontionPageDraft /> */}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
 import './assets/styles/index.css';
 import App from './app/App';
 import './api/authorithation/call-anonym-flow';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
