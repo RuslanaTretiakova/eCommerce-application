7931433b6e81ead6bf40067a0bd677ad1d88b495
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-20 14:05:01 +0200

    refactor: create animation for navigation open button for mobile devices (Tue, May 20, 2025 14:04:28 PM)

--- src/components/header/navigationButton/navigationButton.tsx
+++ src/components/header/navigationButton/navigationButton.tsx
@@ -4,14 +4,21 @@ function NavigationButton() {
   const setMenuOpen = () => {
     let navigationOpen = document.querySelector('.navigation-open');
     const body = document.querySelector('body') as HTMLBodyElement;
+    const button = document.querySelector('.navigation-button') as HTMLElement;
+
     if (!navigationOpen) {
       const navigation = document.querySelector('.navigation');
       navigation?.classList.add('navigation-open');
-
+      button.classList.add('navigation-button_clicked');
+      button.children[0].classList.add('first-line__open');
+      button.children[1].classList.add('second-line__open');
       body.classList.add('no-scroll');
     } else {
       navigationOpen?.classList.remove('navigation-open');
       body.classList.remove('no-scroll');
+      button.children[0].classList.remove('first-line__open');
+      button.children[1].classList.remove('second-line__open');
+      button.classList.remove('navigation-button_clicked');
     }
   };
 
