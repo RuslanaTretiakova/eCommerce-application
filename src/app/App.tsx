f85d6c7add7ac5f10052ee65ac81cf6c4a101882
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-20 13:01:15 +0200

    refactor: change navigation menu style for mobile (Tue, May 20, 2025 13:00:29 PM)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,10 @@ import AuthenticationPage from '../pages/authorisation-page/AuthenticationPage';
 import RegistratontionPageDraft from '../pages/authorisation-page/RegistrationPage-draft';
 import '../assets/styles/normalize.css';
 import '../assets/styles/global.scss';
+import HomePage from '../pages/homePage/homePage';
 
 function App() {
-  return <h1>Hell o </h1>;
+  return <MainPage/>
 }
 
 export default App;
--- src/components/header/navigationButton/navigationButton.tsx
+++ src/components/header/navigationButton/navigationButton.tsx
@@ -3,11 +3,15 @@ import './navigationButton.scss';
 function NavigationButton() {
   const setMenuOpen = () => {
     let navigationOpen = document.querySelector('.navigation-open');
+    const body = document.querySelector('body') as HTMLBodyElement;
     if (!navigationOpen) {
       const navigation = document.querySelector('.navigation');
       navigation?.classList.add('navigation-open');
+   
+      body.classList.add('no-scroll')
     } else {
       navigationOpen?.classList.remove('navigation-open');
+      body.classList.remove('no-scroll')
     }
   };
 
