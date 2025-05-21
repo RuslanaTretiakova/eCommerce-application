6284f5f2de607955cfeb2baa1716fa0d1c0a6d4f
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-21 11:57:46 +0200

    refactor: add style to profile access block (Wed, May 21, 2025 11:57:22 AM)

--- src/components/header/headerProfile/profileAccessBlock/profileAccessBlock.tsx
+++ src/components/header/headerProfile/profileAccessBlock/profileAccessBlock.tsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
-import './profileAccessBlock.scss'
+import './profileAccessBlock.scss';
 
 function ProfileAccessBlock() {
   return (
     <div className="profile-access-block">
-        <h3>Time to ride!</h3>
-      <div className='access-buttons-container'>
-        <p>Already with us? Hit 'Login".</p>
-        <Link to="/login" className='access-button'>Login</Link>
-        
+      <h3>Time to ride!</h3>
+      <div className="access-buttons-container">
+        <p>Already with us? Hit &#39;Login&#39;.</p>
+        <Link to="/login" className="access-button">
+          Login
+        </Link>
+
         <p>First time here? Welcome — registration is just one click away!</p>
         <li>
-          <Link to="/registration" className='access-button'>Registration</Link>
+          <Link to="/registration" className="access-button">
+            Registration
+          </Link>
         </li>
       </div>
     </div>
