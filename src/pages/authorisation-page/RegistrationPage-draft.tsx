a9a2a20241e3a9f7caf503f69f4ab15fa992c06b
Ruslana <tretiakovaruslana7@gmail.com>
2025-05-21 23:12:58 +0200

    feat: add login link and improve registration page styling(Wed, 21 May, 2025 11:12:00 PM)

--- src/pages/authorisation-page/RegistrationPage-draft.tsx
+++ src/pages/authorisation-page/RegistrationPage-draft.tsx
@@ -2,6 +2,9 @@ import type { JSX } from 'react';
 import DynamicForm from '../../components/forms/DynamicForm';
 import { registrationFields } from '../../components/forms/registration/fieldsConfig';
 import type { IFormData } from '../../types/interfaces';
+import { Link } from 'react-router-dom';
+
+import '../../pages/registration-page/ui/_registration-page.scss';
 
 function RegistratontionPageDraft(): JSX.Element {
   const handleRegistration = (data: IFormData) => {
@@ -14,6 +17,12 @@ function RegistratontionPageDraft(): JSX.Element {
         <img src="./src/assets/images/register_page.jpeg.png" alt="Register illustration" />
       </div>
       <div className="registration-page__form">
+        <div className="registration-page__link-to-auth">
+          <p> Already have an account?</p>
+          <Link to="/login" className="button button--login">
+            Login
+          </Link>
+        </div>
         <DynamicForm<IFormData>
           fields={registrationFields}
           onSubmit={handleRegistration}
