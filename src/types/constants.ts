8fa2056277dcb28d21783dd6fcdd4f2445ec695a
Ruslana <tretiakovaruslana7@gmail.com>
2025-05-22 21:43:39 +0200

    chore: add environment variable constants for commercetools config(Thu, 22 May, 2025, 09:43:00 PM)

---
+++ src/types/constants.ts
@@ -0,0 +1,6 @@
+export const CTP_CLIENT_ID = process.env.VITE_CTP_CLIENT_ID as string;
+export const CTP_CLIENT_SECRET = process.env.VITE_CTP_CLIENT_SECRET as string;
+export const CTP_PROJECT_KEY = process.env.VITE_CTP_PROJECT_KEY as string;
+export const CTP_SCOPE = process.env.VITE_CTP_PROJECT_SCOPE as string;
+export const CTP_API_URL = process.env.VITE_CTP_API_URL as string;
+export const CTP_AUTH_URL = process.env.VITE_CTP_API_AUTH as string;
