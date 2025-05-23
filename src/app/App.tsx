f743759dafbcc17abec9be90a73653afb38fba25
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-23 16:09:00 +0200

    chore: update branch from release  (Fri, May 23, 2025 16:05:12 PM)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,6 @@ import NotFoundPage from '../pages/404/404';
 function App() {
   return (
     <Routes>
-      
       <Route path="/" element={<MainPage />}>
         <Route index element={<HomePage />} />
         <Route path="/products" element={<Products />} />
