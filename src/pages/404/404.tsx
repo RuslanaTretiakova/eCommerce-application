b23aab519c8e9a3221b12b3eaf4d2e863b6af4d5
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-23 14:14:15 +0200

    refactor: add button to return to home page  (Fri, May 23, 2025 14:13:22 PM)

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -5,23 +5,27 @@ import Reset from '../../assets/img/reset.svg';
 import { Link } from 'react-router-dom';
 function NotFoundPage() {
   return (
-      <div className="not-found">
-          <div className="title">
-              <p>4</p>
-              <Wheel />
-              <p>4</p>
+    <div className="not-found">
+      <div className="title">
+        <p>4</p>
+        <Wheel />
+        <p>4</p>
       </div>
 
-        <h2 className="title-second">This ride suddenly stopped.&#9940;</h2>
-          <p className="text">
-            Looks like the road ended mid-journey.<img src={ClosedRoad} alt="closed-road" /> 
-          </p> 
+      <h2 className="title-second">This ride suddenly stopped.&#9940;</h2>
+      <p className="text">
+        Looks like the road ended mid-journey.
+        <img src={ClosedRoad} alt="closed-road" />
+      </p>
 
-          <p className="text">
-            No worries — reset your route and try again.<img src={Reset} alt="reset" />
-          </p>
+      <p className="text">
+        No worries — reset your route and try again.
+        <img src={Reset} alt="reset" />
+      </p>
 
-          <Link to="/" className="return-button button">Return to Home</Link>
+      <Link to="/" className="return-button button">
+        Return to Home
+      </Link>
     </div>
   );
 }
