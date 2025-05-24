fdab5c6cf51de57c274c240dcfb32d5ab41744c0
Nikanora <olga.nikanorova@gmail.com>
2025-05-24 16:02:27 +0100

    refactor: add universal constants

--- src/types/constantsCopy.ts
+++ src/types/constantsCopy.ts
@@ -1,6 +1,25 @@
-export const CTP_CLIENT_ID = import.meta.env.VITE_CTP_CLIENT_ID as string;
-export const CTP_CLIENT_SECRET = import.meta.env.VITE_CTP_CLIENT_SECRET as string;
-export const CTP_PROJECT_KEY = import.meta.env.VITE_CTP_PROJECT_KEY as string;
-export const CTP_SCOPE = import.meta.env.VITE_CTP_PROJECT_SCOPE as string;
-export const CTP_API_URL = import.meta.env.VITE_CTP_API_URL as string;
-export const CTP_AUTH_URL = import.meta.env.VITE_CTP_API_AUTH as string;
+const isBrowser = typeof window !== 'undefined';
+
+export const CTP_CLIENT_ID = isBrowser
+  ? (import.meta.env.VITE_CTP_CLIENT_ID as string)
+  : (process.env.VITE_CTP_CLIENT_ID as string);
+
+export const CTP_CLIENT_SECRET = isBrowser
+  ? (import.meta.env.VITE_CTP_CLIENT_SECRET as string)
+  : (process.env.VITE_CTP_CLIENT_SECRET as string);
+
+  export const CTP_PROJECT_KEY = isBrowser
+  ? (import.meta.env.VITE_CTP_PROJECT_KEY as string)
+  : (process.env.VITE_CTP_PROJECT_KEY as string);
+
+  export const CTP_SCOPE = isBrowser
+  ? (import.meta.env.VITE_CTP_PROJECT_SCOPE as string)
+  : (process.env.VITE_CTP_PROJECT_SCOPE as string);
+
+  export const CTP_API_URL = isBrowser
+  ? (import.meta.env.VITE_CTP_API_URL as string)
+  : (process.env.VITE_CTP_API_URL as string);
+
+  export const CTP_AUTH_URL = isBrowser
+  ? (import.meta.env.VITE_CTP_API_AUTH as string)
+  : (process.env.VITE_CTP_API_AUTH as string);
