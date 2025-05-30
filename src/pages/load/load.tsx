5363863fcde8019892adafb48d901e18fac73907
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-30 10:13:17 +0200

    feat: implementation load page to wait for a response (Fri, May 30, 2025 10:13:08 AM)

---
+++ src/pages/load/load.tsx
@@ -0,0 +1,15 @@
+import './load.scss';
+
+function Load() {
+  return (
+    <div className="load-container">
+      <h2 className="load-title">
+        Please hold on while we prepare everything for you. This will only take a moment.
+      </h2>
+
+      <div className="loading-circle" />
+    </div>
+  );
+}
+
+export default Load;
--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from 'react';
+import getProductList from '../../api/getProductList';
+import type { Product } from '@commercetools/platform-sdk';
+import Load from '../load/load';
+
 function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await getProductList();
+        setProducts(response.results);
+        console.log(response.results);
+        console.log(products)
+      } catch (error) {
+        console.error('Failed to load products:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <Load />;
+  }
+
   return (
     <div className="temp">
       <h1>Products page</h1>
