e19d7ed37f6cd3c49467ae94d83b71cffb443972
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-05-31 11:56:12 +0200

    refactor: remove unneccessary code (Sat, May 31, 2025 11:56:12 AM)

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -11,56 +11,8 @@ import Load from '../load/load';
 import type { JSX } from 'react';
 import BaseButton from '../../components/ui/base-button/BaseButton';
 import ProductCard from '../../components/ui/product-card/ProductCard';
-// import type { IProductCard } from '../../types/interfaces';
-
 import './product.scss';
 
-// const products: IProductCard[] = [
-//   {
-//     id: '1',
-//     name: 'Rockrider',
-//     description: 'Rower elektryczny górski MTB Rockrider E-ST100 27,5',
-//     price: '3699,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-//   {
-//     id: '2',
-//     name: 'Romet',
-//     description: 'Rower górski ROMET Rambler R9.3',
-//     price: '1299,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-//   {
-//     id: '3',
-//     name: 'Van Rysel',
-//     description: 'Rower gravelowy dziecięcy Triban GRVL500 26"',
-//     price: '599,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-//   {
-//     id: '4',
-//     name: 'Grundig',
-//     description: 'Rower miejski elektryczny Doc Green by Grundig 28" 14Ah',
-//     price: '9699,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-//   {
-//     id: '5',
-//     name: 'Elops',
-//     description: 'Rower miejski Elops Speed 500',
-//     price: '5699,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-//   {
-//     id: '6',
-//     name: 'Nicebike',
-//     description: 'Rower Górski MTB 29 cali NICEBIKE Triad M6 amortyzacja',
-//     price: '1699,00',
-//     imageUrl: './rower-elektryczny-gorski-mtb-rockrider-e-st100-275.avif',
-//   },
-// ];
-
-// console.log(products);
 
 function Products(): JSX.Element {
   const handleAddToCart = (productId: string) => {
