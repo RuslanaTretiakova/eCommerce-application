36d1bc2c1bb671093f61a6bee85d014e0b670871
Ruslana <tretiakovaruslana7@gmail.com>
2025-06-09 02:29:03 +0200

    feat: add delete button to EditableCard and update styles

--- src/components/ui/editable-card/EditableCard.tsx
+++ src/components/ui/editable-card/EditableCard.tsx
@@ -1,23 +1,36 @@
 import edit from '../../../assets/img/edit-card/edit.svg';
+import trash from '../../../assets/img/edit-card/trash.svg';
 import type { IEditableCard } from '../../../types/interfaces';
 
 import './editable-card.scss';
 
-function EditableCard({ title, onEdit, children }: IEditableCard) {
+function EditableCard({ title, onEdit, onDelete, children }: IEditableCard) {
   return (
     <div className="editable-card">
       <div className="editable-card__header">
         <h3>{title}</h3>
-        {onEdit && (
-          <button
-            type="button"
-            className="editable-card__edit-button"
-            onClick={onEdit}
-            aria-label={`Edit ${title}`}
-          >
-            <img src={edit} alt="edit icon" width={25} height={25} />
-          </button>
-        )}
+        <div className="editable-card__buttons">
+          {onEdit && (
+            <button
+              type="button"
+              className="editable-card__edit-button"
+              onClick={onEdit}
+              aria-label={`Edit ${title}`}
+            >
+              <img src={edit} alt="edit icon" width={25} height={25} />
+            </button>
+          )}
+          {onDelete && (
+            <button
+              type="button"
+              className="editable-card__delete-button"
+              onClick={onDelete}
+              aria-label={`Delete ${title}`}
+            >
+              <img src={trash} alt="delete icon" width={25} height={25} />
+            </button>
+          )}
+        </div>
       </div>
       <div className="editable-card__content">{children}</div>
     </div>
