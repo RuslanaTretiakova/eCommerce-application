a7e5319c067dcf1a3a230db79273ddae0677685f
Ruslana <tretiakovaruslana7@gmail.com>
2025-06-09 04:39:04 +0200

    fix: fix styles

--- src/components/ui/button-reset-filter/button-reset-filter.tsx
+++ src/components/ui/button-reset-filter/button-reset-filter.tsx
@@ -2,7 +2,7 @@ import './button-reset-filter.scss';
 
 function ButtonResetFilter({ onClick }: { onClick: () => void }) {
   return (
-    <button type='button' className="reset-button button" onClick={onClick}>
+    <button type="button" className="reset-button button" onClick={onClick}>
       RESET
     </button>
   );
