f2a0b4f85f0a2965a453cf9d8abb32bda4ac64ca
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-06-15 20:24:18 +0200

    feat:add filter for helmets, fix way to create filter by type (Sun, Jun 15, 2025 20:23:11 PM)

--- src/components/filterCheckbox/filterCheckbox.tsx
+++ src/components/filterCheckbox/filterCheckbox.tsx
@@ -1,41 +1,44 @@
-import { useState } from 'react';
-import './filter-by-type.scss';
+import { useEffect, useState } from 'react';
+import '../ui/filter/filter-by-type.scss';
 
-type Option = {
+export type Option = {
   value: string;
   label: string;
 };
 
-type FilterByTypeProps = {
+interface FilterCheckboxProps {
+  name: string;
+  optionsCheckbox: Option[];
   onChange: (selected: string[]) => void;
-};
+}
 
-const options: Option[] = [
-  { value: 'd3c1ee8c-ae88-4bc6-96d9-a99268ce9c33', label: 'Road Bikes' },
-  { value: '5a05f0ea-a6a3-45f9-9d5f-29e79bb0099d', label: 'E-Bikes' },
-  { value: '0bc1db76-5e11-4148-bc9e-79c1bf65f1bd', label: 'Mountain Bikes' },
-  { value: '772e4169-58e1-4abd-9b02-be79d38ef935', label: 'Folding Bikes' },
-  { value: 'c09916a0-2fab-4882-a2b3-41a4a482b171', label: 'City Bikes' },
-];
-
-function FilterByType({ onChange = () => {} }: FilterByTypeProps) {
+function FilterCheckbox({ name, optionsCheckbox, onChange }: FilterCheckboxProps) {
   const [selectedValues, setSelectedValues] = useState<string[]>([]);
 
+  console.log(optionsCheckbox);
+
   const handleToggleOption = (value: string) => {
-    console.log(value);
     const newSelected = selectedValues.includes(value)
       ? selectedValues.filter((v) => v !== value)
       : [...selectedValues, value];
 
     setSelectedValues(newSelected);
+
     onChange?.(newSelected);
   };
 
+  useEffect(() => {
+    console.log('Updated selectedValues:', selectedValues);
+  }, [selectedValues]);
+
   return (
     <div className="filter-by-type">
-      <h4>Filter by Type</h4>
+      <h4>
+        Filter by
+        {' ' + name}
+      </h4>
       <div className="checkbox-list">
-        {options.map((option) => {
+        {optionsCheckbox.map((option) => {
           const inputId = `filter-${option.value}`;
           return (
             <label key={option.value} htmlFor={inputId} className="checkbox-option">
@@ -54,4 +57,4 @@ function FilterByType({ onChange = () => {} }: FilterByTypeProps) {
   );
 }
 
-export default FilterByType;
+export default FilterCheckbox;
